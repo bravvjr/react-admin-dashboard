f215f0acf67540bdc8dbd8716a138840d69c9aa5
Bravin Murambi <bravvjr@gmail.com>
2025-08-01 15:27:24 +0300

    Adds bar graph

--- src/components/BarChart.jsx
+++ src/components/BarChart.jsx
@@ -11,7 +11,6 @@ const BarChart = ({ isDashboard = false }) => {
     <ResponsiveBar
       data={data}
       theme={{
-        // added
         axis: {
           domain: {
             line: {
@@ -39,13 +38,13 @@ const BarChart = ({ isDashboard = false }) => {
           },
         },
       }}
-      keys={["hot dog", "burger", "sandwich", "kebab", "fries", "donut"]}
-      indexBy="country"
+      keys={["laptops", "accessories", "services"]}
+      indexBy="month"
       margin={{ top: 50, right: 130, bottom: 50, left: 60 }}
       padding={0.3}
       valueScale={{ type: "linear" }}
       indexScale={{ type: "band", round: true }}
-      colors={{ scheme: "nivo" }}
+      colors={{ scheme: 'nivo' }}  // Using original Nivo color scheme
       defs={[
         {
           id: "dots",
@@ -76,7 +75,7 @@ const BarChart = ({ isDashboard = false }) => {
         tickSize: 5,
         tickPadding: 5,
         tickRotation: 0,
-        legend: isDashboard ? undefined : "country", // changed
+        legend: isDashboard ? undefined : "Month",
         legendPosition: "middle",
         legendOffset: 32,
       }}
@@ -84,7 +83,7 @@ const BarChart = ({ isDashboard = false }) => {
         tickSize: 5,
         tickPadding: 5,
         tickRotation: 0,
-        legend: isDashboard ? undefined : "food", // changed
+        legend: isDashboard ? undefined : "Number of Sales",
         legendPosition: "middle",
         legendOffset: -40,
       }}
@@ -121,7 +120,7 @@ const BarChart = ({ isDashboard = false }) => {
       ]}
       role="application"
       barAriaLabel={function (e) {
-        return e.id + ": " + e.formattedValue + " in country: " + e.indexValue;
+        return `${e.id}: ${e.formattedValue} sales in ${e.indexValue}`;
       }}
     />
   );
